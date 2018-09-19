e5264548176c9fba6f87b4164afc67291d109ab6
Brittany Neal <brittanyneal22@gmail.com>
2018-09-19 11:25:39 -0700

    working list files function

---
+++ helper_functions.py
@@ -0,0 +1,11 @@
+import os
+
+directory = '/Users/bneal/Desktop/annotation_tool/test_directory'
+
+def ls_files(directory):
+    """Takes in a directory path and lists all files in the directory."""
+
+    file_names = [file for file in os.listdir(directory)]
+    return(file_names)
+
+
