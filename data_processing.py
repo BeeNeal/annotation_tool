3e6a94cb641b33dd800c13b043816c3a950f602a
Brittany Neal <brittanyneal22@gmail.com>
2018-10-26 10:51:33 -0700

    working text processing from user highlighted annotations

--- data_processing.py
+++ data_processing.py
@@ -34,47 +34,17 @@ def slots_from_labels(label):
 def extract_slot_colors(annotated_line, colors_to_slots):
     """Return list of slots in order of annotation"""
 
+    colors_to_slots = json.loads(colors_to_slots)
     ordered = []
 
-    # in this loop, finding all the indices of #colorcodes in the annotated line, and extract them
-    # need colors_to_slots to be slot:#colorcode format
-    # INSTEAD, return ordered SLOTs and do the lookup conversionhere instead of in the font replace funct
-
-    # Loop over color codes and match them(find index of and extract) with what's in string, preserving the order
-    colors_to_slots = json.loads(colors_to_slots)
-
+    # Loop over color codes and match them(find index of and extract) with
+    # what's in the annotation string, preserving the order
     for color in list(colors_to_slots.keys()):
         color_index = annotated_line.index(color)
         color_code = annotated_line[color_index: color_index + 7]
-        # should now have ordered SLOTS instead of ordered color code
         ordered.append(colors_to_slots[color_code])
-    print(ordered)
 
     return ordered
-
-
-sample_text = """ "qa__ac_enhancement   <font color="#ff4e00">What</font> is the 
-<font color="#8ea604">best</font> way to use <font color="#f5bb00">enhancement mode</font>?" """
-
-processed_once = """ "qa__ac_enhancement   <font color="#ff4e00">WhatTHIS is the 
-<font color="#8ea604">best**** way to use <font color="#f5bb00">enhancement mode#####?" """
-
-sample_dict = {'["acmodel"': '#ff4e00', ' "acmodelnumber"': '#8ea604', ' "actype"]': '#f5bb00'}
-
-
-# def replace_font_with_slot(annotated_line, colors_to_slots):
-#     """Inserts slot name at ending font tags - returns list"""
-#
-#     ordered_slots = extract_slot_colors(annotated_line, colors_to_slots)
-#     new_line = annotated_line
-#
-#     if '</font>' in new_line:
-#         current_slot = ordered_slots.pop(0)
-#         print(current_slot)
-#         new_line = new_line.replace('</font>', ('|<' + current_slot) + '>', 1)
-#         return replace_font_with_slot(new_line, colors_to_slots)
-#
-#     return new_line.split("<font color=")
 
 
 def replace_font_with_slot(annotated_line, colors_to_slots):
@@ -92,15 +62,15 @@ def replace_font_with_slot(annotated_line, colors_to_slots):
 def tag_all_words(text_with_slots):
     """Takes in list of font-tags-replaced-by-slot text; tags remaining words"""
 
-    second_pass = ''
+    completely_tagged = ''
 
     for item in text_with_slots:
         if "|<" in item:
-            second_pass += tag_when_spaces(item)
+            completely_tagged += tag_when_spaces(item)
         else:
-            second_pass += item
+            completely_tagged += item
 
-    return second_pass
+    return completely_tagged
 
 
 def extract_slot_from_line(item):
@@ -134,11 +104,8 @@ def tag_when_spaces(item):
 def process_annotated_text(annotated_line, color_slots_obj):
     """Takes highlighted text from JS, removes font tags, and adds slot tags"""
 
-    # colors_to_slots = json.loads(color_slots_obj)
     colors_to_slots = color_slots_obj
-    # ordered_slots = extract_slot_colors(annotated_line, colors_to_slots)
     first_pass = replace_font_with_slot(annotated_line, colors_to_slots)
-
     tagged_text = tag_all_words(first_pass)
 
     return tagged_text
